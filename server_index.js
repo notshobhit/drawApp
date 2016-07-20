9b4a7fd4e6989d801a311ca1ea5169e63d750cff
Shobhit Verma <svshobhitverma@gmail.com>
2016-07-20 12:41:26 +0530

    Reorganise file structure, setup node server correctly

--- server_index.js
+++ server_index.js
@@ -1,9 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use(express.static('public'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
